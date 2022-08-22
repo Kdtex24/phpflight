6e2041b4999e1ca29c9c94e1e99184ad508f87f5
kdtex24 <kedeayeiokoi@gmail.com>
2022-08-22 12:10:40 +0100

    new files

---
+++ main/footer.php
@@ -0,0 +1,229 @@
+<!-- Footer
+    ============================================= -->
+    <footer id="footer" class="mt-0">
+      <section class="hero-wrap bg-secondary shadow-md pt-4 pb-3">
+        <div class="hero-bg" style="background-image:url('assets/images/bg/image-5.jpg');"></div>
+        <div class="hero-content">
+          <div class="container">
+            <div class="row text-light">
+              <div class="col-sm-6 col-md-3">
+                <div class="featured-box style-1">
+                  <div class="featured-box-icon text-light"> <i class="fas fa-lock"></i> </div>
+                  <h4 class="text-light">100% Secure</h4>
+                  <p>Moving your card details to a much more secured place.</p>
+                </div>
+              </div>
+              <div class="col-sm-6 col-md-3">
+                <div class="featured-box style-1">
+                  <div class="featured-box-icon text-light"> <i class="fas fa-thumbs-up"></i> </div>
+                  <h4 class="text-light">Trust pay</h4>
+                  <p>100% Payment Protection. Easy Return Policy.</p>
+                </div>
+              </div>
+              <div class="col-sm-6 col-md-3">
+                <div class="featured-box style-1">
+                  <div class="featured-box-icon text-light"> <i class="fas fa-bullhorn"></i> </div>
+                  <h4 class="text-light">Refer & Earn</h4>
+                  <p>Invite a friend to sign up and earn up to $100.</p>
+                </div>
+              </div>
+              <div class="col-sm-6 col-md-3">
+                <div class="featured-box style-1">
+                  <div class="featured-box-icon text-light"> <i class="far fa-life-ring"></i> </div>
+                  <h4 class="text-light">24X7 Support</h4>
+                  <p>We're here to help. Have a query and need <a href="#">help</a> ?</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <div class="container mt-4">
+        <div class="row">
+          <div class="col-md-4 mb-3 mb-md-0">
+            <p>Payment</p>
+            <ul class="payments-types">
+              <li><a href="#" target="_blank"> <img data-toggle="tooltip" src="assets/images/payment/visa.png" alt="visa" title="Visa"></a></li>
+              <li><a href="#" target="_blank"> <img data-toggle="tooltip" src="assets/images/payment/discover.png" alt="discover" title="Discover"></a></li>
+              <li><a href="#" target="_blank"> <img data-toggle="tooltip" src="assets/images/payment/paypal.png" alt="paypal" title="PayPal"></a></li>
+              <li><a href="#" target="_blank"> <img data-toggle="tooltip" src="assets/images/payment/american.png" alt="american express" title="American Express"></a></li>
+              <li><a href="#" target="_blank"> <img data-toggle="tooltip" src="assets/images/payment/mastercard.png" alt="discover" title="Discover"></a></li>
+            </ul>
+          </div>
+          <div class="col-md-4 mb-3 mb-md-0">
+            <p>Subscribe</p>
+            <div class="input-group newsletter">
+              <input class="form-control" placeholder="Your Email Address" name="newsletterEmail" id="newsletterEmail" type="text">
+              <span class="input-group-append">
+                <button class="btn btn-secondary" type="submit">Subscribe</button>
+              </span> </div>
+            </div>
+            <div class="col-md-4 d-flex align-items-md-end flex-column">
+              <p>Keep in touch</p>
+              <ul class="social-icons social-icons-colored">
+                <li class="social-icons-facebook"><a data-toggle="tooltip" href="http://www.facebook.com/" target="_blank" title="Facebook"><i class="fab fa-facebook-f"></i></a></li>
+                <li class="social-icons-twitter"><a data-toggle="tooltip" href="http://www.twitter.com/" target="_blank" title="Twitter"><i class="fab fa-twitter"></i></a></li>
+                <li class="social-icons-google"><a data-toggle="tooltip" href="http://www.google.com/" target="_blank" title="Google"><i class="fab fa-google"></i></a></li>
+                <li class="social-icons-linkedin"><a data-toggle="tooltip" href="http://www.linkedin.com/" target="_blank" title="Linkedin"><i class="fab fa-linkedin-in"></i></a></li>
+                <li class="social-icons-youtube"><a data-toggle="tooltip" href="http://www.youtube.com/" target="_blank" title="Youtube"><i class="fab fa-youtube"></i></a></li>
+                <li class="social-icons-instagram"><a data-toggle="tooltip" href="http://www.instagram.com/" target="_blank" title="Instagram"><i class="fab fa-instagram"></i></a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="container">
+          <div class="footer-copyright">
+            <ul class="nav justify-content-center">
+              <li class="nav-item"> <a class="nav-link active" href="#">About Us</a> </li>
+              <li class="nav-item"> <a class="nav-link" href="#">Faq</a> </li>
+              <li class="nav-item"> <a class="nav-link" href="#">Contact</a> </li>
+              <li class="nav-item"> <a class="nav-link" href="#">Support</a> </li>
+              <li class="nav-item"> <a class="nav-link" href="#">Terms of Use</a> </li>
+              <li class="nav-item"> <a class="nav-link" href="#">Privacy Policy</a> </li>
+            </ul>
+            <p class="copyright-text">Copyright © 2022 <a href="#">kdtex Travels</a>. All Rights Reserved.</p>
+          </div>
+        </div>
+      </footer>
+      <!-- Footer end --> 
+      
+    </div>
+    <!-- Document Wrapper end --> 
+
+<!-- Back to Top
+  ============================================= --> 
+  <a id="back-to-top" data-toggle="tooltip" title="Back to Top" href="javascript:void(0)"><i class="fa fa-chevron-up"></i></a> 
+
+<!-- Modal Dialog - Login/Signup
+  ============================================= -->
+  <div id="login-signup" class="modal fade" role="dialog" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+      <div class="modal-content p-sm-3">
+        <div class="modal-body">
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close"> <span aria-hidden="true">&times;</span> </button>
+          <ul class="nav nav-tabs" role="tablist">
+            <li class="nav-item"> <a id="login-tab" class="nav-link active text-4" data-toggle="tab" href="#login" role="tab" aria-controls="login" aria-selected="true">Login</a> </li>
+            <li class="nav-item"> <a id="signup-tab" class="nav-link text-4" data-toggle="tab" href="#signup" role="tab" aria-controls="signup" aria-selected="false">Sign Up</a> </li>
+          </ul>
+          <div class="tab-content pt-4">
+            <div class="tab-pane fade show active" id="login" role="tabpanel" aria-labelledby="login-tab">
+              <form id="loginForm" method="post">
+                <div class="form-group">
+                  <input type="email" class="form-control" id="loginMobile" required placeholder="Mobile or Email ID">
+                </div>
+                <div class="form-group">
+                  <input type="password" class="form-control" id="loginPassword" required placeholder="Password">
+                </div>
+                <div class="row mb-4">
+                  <div class="col-sm">
+                    <div class="form-check custom-control custom-checkbox">
+                      <input id="remember-me" name="remember" class="custom-control-input" type="checkbox">
+                      <label class="custom-control-label" for="remember-me">Remember Me</label>
+                    </div>
+                  </div>
+                  <div class="col-sm text-right"> <a class="justify-content-end" href="#">Forgot Password ?</a> </div>
+                </div>
+                <button class="btn btn-primary btn-block" type="submit">Login</button>
+              </form>
+            </div>
+            <div class="tab-pane fade" id="signup" role="tabpanel" aria-labelledby="signup-tab">
+              <form id="signupForm" method="post">
+                <div class="form-group">
+                  <input type="text" class="form-control" data-bv-field="number" id="signupEmail" required placeholder="Email ID">
+                </div>
+                <div class="form-group">
+                  <input type="text" class="form-control" id="signupMobile" required placeholder="Mobile Number">
+                </div>
+                <div class="form-group">
+                  <input type="password" class="form-control" id="signuploginPassword" required placeholder="Password">
+                </div>
+                <button class="btn btn-primary btn-block" type="submit">Signup</button>
+              </form>
+            </div>
+            <div class="d-flex align-items-center my-4">
+              <hr class="flex-grow-1">
+              <span class="mx-2">OR</span>
+              <hr class="flex-grow-1">
+            </div>
+            <div class="row">
+              <div class="col-12 mb-3">
+                <button type="button" class="btn btn-block btn-outline-primary">Login with Facebook</button>
+              </div>
+              <div class="col-12">
+                <button type="button" class="btn btn-block btn-outline-danger">Login with Google</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div><!-- Modal Dialog - Login/Signup end --> 
+
+  <!-- Script --> 
+  <script src="assets/vendor/jquery/jquery.min.js"></script> 
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script> 
+  <script src="assets/vendor/owl.carousel/owl.carousel.min.js"></script> 
+  <script src="assets/vendor/bootstrap-spinner/bootstrap-spinner.js"></script> 
+  <script src="assets/vendor/daterangepicker/moment.min.js"></script> 
+  <script src="assets/vendor/daterangepicker/daterangepicker.js"></script> 
+  <script>
+    $(function() {
+     'use strict';
+  // Hotels Check In Date
+  $('#hotelsCheckIn').daterangepicker({
+    singleDatePicker: true,
+    minDate: moment(),
+    autoUpdateInput: false,
+  }, function(chosen_date) {
+    $('#hotelsCheckIn').val(chosen_date.format('MM-DD-YYYY'));
+  });
+  
+  // Hotels Check Out Date
+  $('#hotelsCheckOut').daterangepicker({
+    singleDatePicker: true,
+    minDate: moment(),
+    autoUpdateInput: false,
+  }, function(chosen_date) {
+    $('#hotelsCheckOut').val(chosen_date.format('MM-DD-YYYY'));
+  });
+  
+  // Flight Depart Date
+  $('#flightDepart').daterangepicker({
+    singleDatePicker: true,
+    minDate: moment(),
+    autoUpdateInput: false,
+  }, function(chosen_date) {
+    $('#flightDepart').val(chosen_date.format('MM-DD-YYYY'));
+  });
+  
+  // Flight Return Date
+  $('#flightReturn').daterangepicker({
+    singleDatePicker: true,
+    minDate: moment(),
+    autoUpdateInput: false,
+  }, function(chosen_date) {
+    $('#flightReturn').val(chosen_date.format('MM-DD-YYYY'));
+  });
+  
+  // Train Depart Date
+  $('#trainDepart').daterangepicker({
+    singleDatePicker: true,
+    minDate: moment(),
+    autoUpdateInput: false,
+  }, function(chosen_date) {
+    $('#trainDepart').val(chosen_date.format('MM-DD-YYYY'));
+  });
+  
+  // Bus Depart Date
+  $('#busDepart').daterangepicker({
+    singleDatePicker: true,
+    minDate: moment(),
+    autoUpdateInput: false,
+  }, function(chosen_date) {
+    $('#busDepart').val(chosen_date.format('MM-DD-YYYY'));
+  });
+});
+</script> 
+<script src="assets/js/theme.js"></script>
+</body>
+</html>
---
+++ main/header.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0, shrink-to-fit=no">
+  <link href="assets/images/faviconnew.png" rel="icon" />
+  <title>kdtex travels</title>
+  <meta name="description" content="Flights,htoels and travel booking agency website">
+  <meta name="author" content="kdtex technologies">
+
+<!-- Web Fonts
+  ============================================= -->
+  <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900' type='text/css'>
+
+<!-- Stylesheet
+  ============================================= -->
+  <link rel="stylesheet" type="text/css" href="assets/vendor/bootstrap/css/bootstrap.min.css" />
+  <link rel="stylesheet" type="text/css" href="assets/vendor/font-awesome/css/all.min.css" />
+  <link rel="stylesheet" type="text/css" href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" />
+  <link rel="stylesheet" type="text/css" href="assets/vendor/owl.carousel/assets/owl.theme.default.min.css" />
+  <link rel="stylesheet" type="text/css" href="assets/vendor/daterangepicker/daterangepicker.css" />
+  <link rel="stylesheet" type="text/css" href="assets/css/stylesheet.css" />
+</head>
+<body>
+  <!-- Preloader -->
+  <div id="preloader"><div data-loader="dual-ring"></div></div><!-- Preloader End -->
+
+<!-- Document Wrapper   
+  ============================================= -->
+  <div id="main-wrapper"> 
+
+  <!-- Header
+    ============================================= -->
+    <header id="header">
+      <div class="container">
+        <div class="header-row">
+          <div class="header-column justify-content-start"> 
+
+          <!-- Logo
+            ============================================= -->
+            <div class="logo"> <a href="index.html" title="kdtex travels"><img src="..assets/images/logonew.png" alt="kdtex travels" width="127" height="29" /></a> </div>
+            <!-- Logo end --> 
+            
+          </div>
+          <div class="header-column justify-content-end"> 
+
+          <!-- Primary Navigation
+            ============================================= -->
+            <nav class="primary-menu navbar navbar-expand-lg nav-primary-dropdown">
+              <div id="header-nav" class="collapse navbar-collapse">
+                <ul class="navbar-nav">
+                  <li class=""> <a class="" href="index.html">Home</a>
+                  </li>
+                <li class="login-signup ml-lg-2"><a class="pl-lg-4 pr-0" data-toggle="modal" data-target="#login-signup" href="#" title="Login / Sign up">Book Flight <span class="d-none d-lg-inline-block"><i class="fas fa-user"></i></span></a></li>
+              </ul>
+            </div>
+          </nav>
+          <!-- Primary Navigation end --> 
+        </div>
+        
+        <!-- Collapse Button
+          ============================================= -->
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#header-nav"> <span></span> <span></span> <span></span> </button>
+        </div>
+      </div>
+    </header>
+    <!-- Header end --> 
